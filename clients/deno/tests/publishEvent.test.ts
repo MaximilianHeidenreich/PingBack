6c6f2fdeab1246406ed18dbebbc4ea3bdde93577
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-02-25 22:59:09 +0100

    👷 Add test push action

--- clients/deno/tests/publishEvent.test.ts
+++ clients/deno/tests/publishEvent.test.ts
@@ -1,14 +1,34 @@
 import { PingBack } from "../mod.ts";
 
 const pingback = new PingBack({
-    apiKey: "d4bb6a94-76eb-46a1-b7d2-6d08937f89b4",
-    appUrl: "http://localhost:5173",
-    project: "test-project",
+    apiKey: "5770eebb-3d77-4adc-bd07-77ab38f35b51",
+    appUrl: "https://pingback-1-d7329150.deta.app",
+    project: "deta",
 });
 
-await pingback.publishEvent({
+/*await pingback.publishEvent({
     channel: "general",
     event: "test",
     title: "Hello World from Deno SDK",
     icon: "🦕",
+});*/
+
+
+/*try {
+    throw new Error("Some error");
+}
+catch (err) {
+    // @ts-ignore
+}*/
+
+await pingback.publishEvent({
+    channel: "pingback",
+    event: "push.sucess",
+    title: "Pushed PingBack to DetaSpace",
+    description: "",
+    tags: {
+        
+    },
+    parser: "text",
+    icon: "🪐",
 });
