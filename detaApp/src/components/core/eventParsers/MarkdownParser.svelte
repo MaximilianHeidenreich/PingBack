53bd773f66747df882905ee289c04cb251bf2eb8
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-02-26 13:55:34 +0100

    ⚰️ Cleanup

--- detaApp/src/components/core/eventParsers/MarkdownParser.svelte
+++ detaApp/src/components/core/eventParsers/MarkdownParser.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
     import type { IEvent } from "$lib/types/IEvent";
-    import { compile } from "mdsvex";
 
     // PROPS
     export let event: IEvent;
-
-    // STATE
-    /*let compiled = compile(event.description, {
-        smartypants: true,
-    })*/
 </script>
 
 <div>
