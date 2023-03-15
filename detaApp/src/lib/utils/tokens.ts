4282ad37b4c680aef1cdc0ba510d3fed9afbee48
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-15 01:52:07 +0100

    add: design tokens constants

---
+++ detaApp/src/lib/utils/tokens.ts
@@ -0,0 +1,19 @@
+import { quintOut } from "svelte/easing";
+
+export const TKN_ICON = Object.freeze({
+    SIZE: {
+        SM: 18,
+        BASE: 24,
+        LG: 30
+    },
+    STROKE: {
+        SM: 1.5,
+        BASE: 2,
+        LG: 2.5
+    },
+});
+
+export const TKN_TRANSITION = Object.freeze({
+    DURATION: 350,
+    EASING: quintOut
+});
