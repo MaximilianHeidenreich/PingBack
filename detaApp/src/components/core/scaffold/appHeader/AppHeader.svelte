503fc89be4fcc843309092d089080e57fc592f6c
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-17 17:05:37 +0100

    add: AppHeader & stores

--- detaApp/src/components/core/scaffold/appHeader/AppHeader.svelte
+++ detaApp/src/components/core/scaffold/appHeader/AppHeader.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
     import IconButton from "$cmp/core/buttons/IconButton.svelte";
+    import { s_eventListStyle } from "$cmp/eventList/s_eventListStyle";
     import { TKN_ICON } from "$lib/utils/tokens";
-    import { IconMenu2 } from "@tabler/icons-svelte";
+    import { IconMenu2, IconRefresh, IconSearch, IconStack2 } from "@tabler/icons-svelte";
+    import { get } from "svelte/store";
+    import SearchBar from "./SearchBar.svelte";
+    import { s_headerLoading } from "./s_headerLoading";
+    import { s_headerTitle } from "./s_headerTitle";
+    import { s_refresh } from "./s_refresh";
 
+    // STATE
+    let searchOpen = false;
+
+    // HANDLERS
+    function onEnterSearch() {
+        searchOpen = true;
+    }
+    function onToggleListStyle() {
+        const curr = get(s_eventListStyle);
+        if (curr === "compact") s_eventListStyle.set("card");
+        else if (curr === "card") s_eventListStyle.set("compact");
+    }
+    function onRefresh() {
+        //s_refresh.set(!get(s_refresh));
+    }
 </script>
 
 <header>
@@ -12,10 +33,41 @@
             stroke={TKN_ICON.STROKE.BASE} />
     </IconButton>
     <div class="flex-1 grow flex justify-between items-center">
-        <span class="page-title">Page Title</span>
-        <ul class="flex-1 grow flex justify-end items-center">
-            <slot/>
-            asd
+        {#if !searchOpen}
+        <span class="page-title">{$s_headerTitle}</span>
+        {/if}
+        <ul class="flex-1 grow flex justify-end items-center gap-2">
+            <li class="{searchOpen ? 'flex-1' : ''}">
+                {#if searchOpen}
+                <SearchBar />
+                {:else}
+                <IconButton on:click={onEnterSearch}>
+                    <IconSearch
+                        size={TKN_ICON.SIZE.BASE}
+                        stroke={TKN_ICON.STROKE.BASE} />
+                </IconButton>
+                {/if}
+            </li>
+            <li>
+                <IconButton on:click={onToggleListStyle}>
+                    <IconStack2
+                        size={TKN_ICON.SIZE.BASE}
+                        stroke={TKN_ICON.STROKE.BASE} />
+                </IconButton>
+            </li>
+            <li>
+                {#key $s_refresh}
+                <IconButton on:click={onRefresh}>
+                    {#key $s_headerLoading}
+                    <IconRefresh
+                        size={TKN_ICON.SIZE.BASE}
+                        stroke={TKN_ICON.STROKE.BASE}
+                        class={get(s_headerLoading) ? "animate-spin" : ""}
+                        style="animation-direction: reverse;" />
+                    {/key}
+                </IconButton>
+                {/key}
+            </li>
         </ul>
     </div>
 </header>
---
+++ detaApp/src/components/core/scaffold/appHeader/SearchBar.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<div class="flex-1 bg-red-100 py-2">
+    search
+</div>
