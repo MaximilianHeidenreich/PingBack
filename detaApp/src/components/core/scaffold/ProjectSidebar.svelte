86d6c5b8f33e7414f03f5f0e74d48fb569c6626d
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-17 17:05:19 +0100

    add: projectaside: channels & links

--- detaApp/src/components/core/scaffold/ProjectSidebar.svelte
+++ detaApp/src/components/core/scaffold/ProjectSidebar.svelte
@@ -1,19 +1,31 @@
 <script lang="ts">
-    import { TKN_ICON } from "$lib/utils/tokens";
-    import { IconBell, IconChartHistogram, IconRss, IconSettings } from "@tabler/icons-svelte";
+    import { TKN_ICON, TKN_TRANSITION } from "$lib/utils/tokens";
+    import { IconBell, IconChartHistogram, IconHash, IconPlus, IconRss, IconSettings } from "@tabler/icons-svelte";
     import IconButton from "../buttons/IconButton.svelte";
+    import { fly } from "svelte/transition";
 
     import "$css/menuLinks.postcss";
+    import { s_projectSidebarActiveProject } from "./s_projectSidebarActiveProject";
+    import { page } from "$app/stores";
+
+    // STATE
+    $: BASE_URL = `/app/project/${$s_projectSidebarActiveProject?.key}`;
 
 </script>
 
-<div class="project-nav">
+{#if $s_projectSidebarActiveProject !== undefined}
+<div class="project-nav"
+    transition:fly={{
+        duration: TKN_TRANSITION.DURATION,
+        easing: TKN_TRANSITION.EASING,
+        x: -100
+    }}>
     <header class="mb-12">
-        <div class="flex items-center justify-between gap-6">
-            <span>Test Project</span>
+        <div class="flex items-center justify-between gap-4 shrink-0">
+            <span class="text-lg font-medium truncate">{$s_projectSidebarActiveProject?.displayName}</span>
             <IconButton
-                type="button"
-                href="/settings">
+                type="link"
+                href="{BASE_URL}/settings">
                 <IconSettings
                     size={TKN_ICON.SIZE.BASE}
                     stroke={TKN_ICON.STROKE.BASE} />
@@ -24,24 +36,24 @@
         <ul class="m-links">
             <li>
                 <a
-                    href="/feed"
-                    class:active={false}
+                    href="{BASE_URL}/feed"
+                    class:active={$page.url.pathname.endsWith(`feed`)}
                     ><IconRss
                         size={TKN_ICON.SIZE.SM}
                         stroke={TKN_ICON.STROKE.BASE} /> Feed</a>
             </li>
             <li>
                 <a
-                    href="/metrics"
-                    class:active={false}
+                    href="{BASE_URL}/metrics"
+                    class:active={$page.url.pathname.endsWith(`metrics`)}
                     ><IconChartHistogram
                         size={TKN_ICON.SIZE.SM}
                         stroke={TKN_ICON.STROKE.BASE} /> Metrics</a>
             </li>
             <li>
                 <a
-                    href="/monitor"
-                    class:active={false}
+                    href="{BASE_URL}/monitor"
+                    class:active={$page.url.pathname.endsWith(`monitor`)}
                     ><IconBell
                         size={TKN_ICON.SIZE.SM}
                         stroke={TKN_ICON.STROKE.BASE} /> Monitor</a>
@@ -49,11 +61,48 @@
         </ul>
         <hr class="mt-5" />
     </section>
+    <section>
+        <header class="flex items-center justify-between gap-6">
+            <span class="title">Channels</span>
+            <IconButton
+                type="button"
+                on:click={() => {}}>
+                <IconPlus
+                    size={TKN_ICON.SIZE.BASE}
+                    stroke={TKN_ICON.STROKE.BASE} />
+            </IconButton>
+        </header>
+        <ul class="m-links mt-1">
+            {#each $s_projectSidebarActiveProject.channels as channel}
+                <li>
+                    <a
+                        href="{BASE_URL}/channel/{channel.id}"
+                        class="font-mono"
+                        class:active={$page.url.pathname.endsWith(`channel/${channel.id}`)}>
+                        <IconHash
+                            class="-mr-2 shrink-0"
+                            size={TKN_ICON.SIZE.SM}
+                            stroke={TKN_ICON.STROKE.BASE} />
+                        <span class="max-w-[15ch] truncate">{channel.id}</span>
+                    </a>
+                </li>
+            {/each}
+        </ul>
+    </section>
 </div>
+{/if}
 
 <style lang="postcss">
     .project-nav {
-        @apply bg-white bg-neutral-50;
         @apply px-6 py-6 border-r-4;
+        @apply bg-white;
+        @apply min-w-[20ch] max-w-[24ch];
+    }
+
+    section {
+        @apply mb-4;
+    }
+    section header span.title {
+        @apply text-lg font-medium;
     }
 </style>
