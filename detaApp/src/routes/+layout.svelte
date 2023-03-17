77b04478debe54689fee103775b2a991a7b604b5
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-17 02:32:10 +0100

    add: test data generator

--- detaApp/src/routes/+layout.svelte
+++ detaApp/src/routes/+layout.svelte
@@ -1,8 +1,76 @@
 <script lang="ts">
-    import { Toaster } from "svelte-french-toast";
-
+    //import { db_events } from "$lib/server/deta";
+    import type { IEvent } from "$lib/types/IEvent";
+    import dayjs from "dayjs";
     import "../css/app.postcss";
+
+
+    // TODO: REMOVE; DEV ONLY!
+    const dev_events: IEvent[] = [
+        {
+            key: crypto.randomUUID(),
+            v: 1,
+            createdAt: dayjs().valueOf(),
+            project: "test-project",
+            channel: "general",
+            eventName: "test.first",
+            notify: true,
+            icon: "🔔",
+            parser: "text",
+            title: `General 1 - ${dayjs().format("DD-MM-YYYY HH:mm:ss")}`,
+            description: "Test description",
+            tags: {
+                "foo": "bar",
+                "bat": "baz"
+            }
+        },
+        {
+            key: crypto.randomUUID(),
+            v: 1,
+            createdAt: dayjs().subtract(1, "minute").valueOf(),
+            project: "test-project",
+            channel: "general",
+            eventName: "test.first",
+            notify: true,
+            icon: "🔔",
+            parser: "text",
+            title: `General 2 - ${dayjs().subtract(1, "minute").format("DD-MM-YYYY HH:mm:ss")}`,
+            description: "Test description",
+            tags: {
+                "foo": "bar",
+                "bat": "baz"
+            }
+        },
+    ];
+    const f = (arr: any[], hour: number, items: number = 5) => {
+        for (let i = 1; i <= items; i++) {
+            arr.push({
+                key: crypto.randomUUID(),
+                v: 1,
+                createdAt: dayjs().subtract(hour, "hour").subtract(3, "minute").valueOf(),
+                project: "test-project",
+                channel: "general",
+                eventName: "test.first",
+                notify: true,
+                icon: "🔔",
+                parser: "text",
+                title: `General -${hour}h 3 - ${dayjs().subtract(hour, "hour").subtract(i, "minute").format("DD-MM-YYYY HH:mm:ss")}`,
+                description: "Test description",
+                tags: {
+                    "foo": "bar",
+                    "bat": "baz"
+                }
+            },);
+        }
+    }
+    for (let x = 1; x < 50; x++) {
+        f(dev_events, x, 120);
+    }
+    //db_events.putMany(dev_events);
+    dev_events.forEach(e => {
+        //db_events.put(e, e.key);
+    })
+
 </script>
 
-<Toaster />
 <slot />
