d17290a9b780b7894e7c620fc1b39e3630e87c21
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-17 17:07:25 +0100

    cleanup & add: contentHash

--- detaApp/src/lib/types/IProject.ts
+++ detaApp/src/lib/types/IProject.ts
@@ -7,9 +7,9 @@ export interface IProject {
     // META
     key: string; // Unique ID
     createdAt: number; // Unix timestamp
+    contentHash: string; // "Hash" of project -> Change inavlidates client cahces
 
     // SYSTEM META -> read only OR know what you're doing!
-    firstEventTimestamp: number; // Unix timestamp of first event in project -> query know when to stop
     latestEventTimestamp: number; // Unix timestamp of latest event in project -> efficient query by starting at latest
     largestEventTimestampRange: number; // Largest difference between events in project -> efficient query by maximixing query range for less queries
     eventSpecifiers: Record<string, number>; // Map of event names to number of events of its kind -> user filter ux
