4eee1ecbeaf11e54d9a3b979bd176d64947cb6a2
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-17 20:32:36 +0100

    add: Input cmp

---
+++ detaApp/src/components/core/inputs/Input.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    // TODO: REVIEW & REWRITE?
+    // PROPS
+    export let type: "text" = "text",
+        value: string = "",
+        sanitizer: (value: string) => string = (value) => value,
+        placeholder: string = "";
+
+    // HANDLERS
+    const onInput = () => (value = sanitizer(value));
+</script>
+
+{#if type === "text"}
+    <input
+        type="text"
+        bind:value
+        on:input={onInput}
+        {placeholder} />
+{/if}
+
+<style lang="postcss">
+    input {
+        @apply mt-1 block w-full rounded-md border-gray-300 shadow-sm;
+    }
+    input:focus {
+        @apply border-pink-500 ring ring-neutral-400 ring-opacity-0;
+    }
+</style>
