fccf622a11d8b6950a1fed0c8dc1326d305a2f9c
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-18 11:36:00 +0100

    🤡 Mock stats & settings pages

--- detaApp/src/routes/app/settings/+page.svelte
+++ detaApp/src/routes/app/settings/+page.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+    import { s_headerTitle } from "$cmp/core/scaffold/appHeader/s_headerTitle";
+
+    s_headerTitle.set("Global Settings");
+</script>
 
 <section>
     <header>
---
+++ detaApp/src/routes/app/stats/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { s_headerTitle } from "$cmp/core/scaffold/appHeader/s_headerTitle";
+    import { s_projectSidebarActiveProject } from "$cmp/core/scaffold/s_projectSidebarActiveProject";
+
+    s_headerTitle.set("Global Stats");
+    s_projectSidebarActiveProject.set(null);
+</script>
+
+<section>
+    <header>
+        <span class="title">coming soon</span>
+    </header>
+
+</section>
