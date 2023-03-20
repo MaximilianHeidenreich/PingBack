51e9eab2c6d67b6d708a084d81f09934d48d0f3c
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-20 20:01:21 +0100

    💄 Add loading ui

--- detaApp/src/routes/+page.svelte
+++ detaApp/src/routes/+page.svelte
@@ -1,23 +1,15 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
+    import Spinner from "$cmp/core/utils/Spinner.svelte";
     import { onMount } from "svelte";
 
     // HOOKS
     onMount(() => {
         // Initial setup call to api -> Sets up system config etc.
-
         goto("/app", { replaceState: true });
     });
 </script>
 
 <div class="flex h-full w-full items-center justify-center">
-    <!--<div class="animate-spin p-1">
-        <IconFidgetSpinner
-            size={ICON_TOKENS.SIZE.LG}
-            stroke={ICON_TOKENS.STROKE.BASE} />
-    </div>-->
-    todo laoding
-</div>
-<div class="w-full bg-red-100">
-foo
+    <Spinner/>
 </div>
