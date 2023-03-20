e6c707888e69fcadf42a33fab21a6486dc39aaea
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-20 18:44:23 +0100

    👔 Add&Fix create & delete project modal

--- detaApp/src/components/modalContents/ModalCreateProject.svelte
+++ detaApp/src/components/modalContents/ModalCreateProject.svelte
@@ -2,18 +2,51 @@
     import Button from "$cmp/core/buttons/Button.svelte";
     import Input from "$cmp/core/inputs/Input.svelte";
     import Modal from "$cmp/core/modals/Modal.svelte";
+    import { s_appSidebarProjects } from "$cmp/core/scaffold/s_appSidebarProjects";
+    import { clientCreateProjectRaw } from "$lib/helpers/api/projectsClient";
+    import type { IProject } from "$lib/types/IProject";
     import { sanitizeProjectIdInput } from "$lib/utils/sanitizers";
+    import toastOptions from "$lib/utils/toast";
+    import toast from "svelte-french-toast";
 
     // STATE
     let dialog: HTMLDialogElement;
     let projectName: string;
 
+    // FN
+    function reset() {
+        projectName = "";
+    }
+
     // HANDLERS
     function onCancel() {
         dialog.close();
     }
-    function onCreate() {
+    async function onCreate() {
+        const res = await clientCreateProjectRaw(fetch, {
+            key: sanitizeProjectIdInput(projectName),
+            displayName: projectName,
+        });
+        console.debug("Create project fetch result", res);
 
+        if (res.status === 200) {
+            toast.success("Created project!", toastOptions());
+            dialog.close();
+            const body = (await res.json()) as IProject;
+            s_appSidebarProjects.update(projects => {
+                projects = [...projects, body];
+                return projects;
+            });
+            return;
+        }
+        else if (res.status === 400) {
+            toast.error("Could not create project!", toastOptions());
+        }
+        else if (res.status === 409) {
+            toast.error("Prokect already exists!", toastOptions());
+        }
+
+        reset();
     }
 </script>
 
@@ -42,7 +75,7 @@
     <svelte:fragment slot="footer">
     <div class="flex justify-end gap-4">
         <Button style="secondary" on:click={onCancel}>Cancel</Button>
-        <Button>Create</Button>
+        <Button on:click={onCreate}>Create</Button>
     </div>
     </svelte:fragment>
 </Modal>
---
+++ detaApp/src/components/modalContents/ModalDeleteProject.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import Button from "$cmp/core/buttons/Button.svelte";
+    import Modal from "$cmp/core/modals/Modal.svelte";
+    import { clientDeleteProjectRaw } from "$lib/helpers/api/projectsClient";
+    import toastOptions from "$lib/utils/toast";
+    import toast from "svelte-french-toast";
+
+    // PROPS
+    export let projectID: string,
+        projectName: string;
+
+    // STATE
+    let dialog: HTMLDialogElement;
+
+    // HANDLERS
+    function onCancel() {
+        dialog.close();
+    }
+    async function onDelete() {
+        const res = await clientDeleteProjectRaw(fetch, projectID);
+        console.debug("Delete project fetch result", res);
+
+        if (res.status === 200) {
+            toast.success("Deleted project!", toastOptions());
+        }
+        else {
+            toast.error("Could not delete project!", toastOptions());
+        }
+        dialog.close();
+    }
+</script>
+
+<Modal bind:dialog>
+    <svelte:fragment slot="title">Delete Project</svelte:fragment>
+    <svelte:fragment slot="subtitle">
+        Are you sure you want to delete the project <strong class="text-pink-500">{projectName}</strong>?
+        This action cannot be reversed and all data will be lost.
+    </svelte:fragment>
+    <svelte:fragment slot="footer">
+    <div class="flex justify-end gap-4">
+        <Button style="secondary" on:click={onCancel}>Cancel</Button>
+        <Button on:click={onDelete} color="red">Delete</Button>
+    </div>
+    </svelte:fragment>
+</Modal>
