6d1d90480af42ec179ce505442d5786a93450ff2
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-25 15:46:04 +0100

    🐛 small fixes

--- detaApp/src/routes/app/+page.svelte
+++ detaApp/src/routes/app/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
     import AppContentSection from "$cmp/core/scaffold/AppContentSection.svelte";
     import { s_headerTitle } from "$cmp/core/scaffold/appHeader/s_headerTitle";
-    import { s_projectSidebarActiveProject } from "$cmp/core/scaffold/s_projectSidebarActiveProject";
     import { onMount } from "svelte";
 
     s_headerTitle.set("Overview");
 
     onMount(() => {
-        s_projectSidebarActiveProject.set(null);
+
     });
 </script>
 
