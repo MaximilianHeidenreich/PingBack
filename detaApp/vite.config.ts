68461cd667e3b64c9259f83d61bef9a006f5c144
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-25 21:52:57 +0100

    🔧 PWA & -pouchdb

--- detaApp/vite.config.ts
+++ detaApp/vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { SvelteKitPWA } from "@vite-pwa/sveltekit";
+//import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 import { readFileSync } from "fs";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
@@ -9,7 +9,7 @@ const json = readFileSync(file, "utf8");
 const pkg = JSON.parse(json);
 
 export default defineConfig({
-    plugins: [sveltekit(), SvelteKitPWA()],
+    plugins: [sveltekit()], // SvelteKitPWA()
     define: {
         __VERSION__: JSON.stringify(pkg.version)
     }
