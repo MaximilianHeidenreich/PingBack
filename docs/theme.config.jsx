8aefa024f20fb5bc0c83bffd0b1188d8eb63db7a
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-25 15:39:49 +0100

    📝 Add docs micro project

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,22 +1,35 @@
 export default {
     logo: (
         <>
-        <svg width="24" height="24" viewBox="0 0 24 24">
-        <path
-        fill="currentColor"
-        d="M14.683 14.828a4.055 4.055 0 0 1-1.272.858a4.002 4.002 0 0 1-4.875-1.45l-1.658 1.119a6.063 6.063 0 0 0 1.621 1.62a5.963 5.963 0 0 0 2.148.903a6.035 6.035 0 0 0 3.542-.35a6.048 6.048 0 0 0 1.907-1.284c.272-.271.52-.571.734-.889l-1.658-1.119a4.147 4.147 0 0 1-.489.592z M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10s10-4.486 10-10S17.514 2 12 2zm0 2c2.953 0 5.531 1.613 6.918 4H5.082C6.469 5.613 9.047 4 12 4zm0 16c-4.411 0-8-3.589-8-8c0-.691.098-1.359.264-2H5v1a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2h2a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-1h.736c.166.641.264 1.309.264 2c0 4.411-3.589 8-8 8z"
-        />
-        </svg>
-        <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
-        <span style={{ color: "#ea4a8f" }}>Ping</span>Back
-        </span>
+            <svg width="30" height="30" viewBox="0 0 167 167" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <mask id="mask0_4_732" style={{ maskType: "alpha" }} maskUnits="userSpaceOnUse" x="0" y="0" width="167" height="167">
+                <path d="M108.777 0H58.2232C41.4578 0 33.0757 0 24.0522 2.8529C14.1992 6.43926 6.43924 14.1992 2.85292 24.0522C0 33.0757 0 41.4591 0 58.2232V108.777C0 125.542 0 133.923 2.85292 142.948C6.43924 152.801 14.1992 160.561 24.0522 164.146C33.0757 167 41.4578 167 58.2232 167H108.777C125.542 167 133.923 167 142.948 164.146C152.801 160.561 160.561 152.801 164.147 142.948C167 133.923 167 125.542 167 108.777V58.2232C167 41.4591 167 33.0757 164.147 24.0522C160.561 14.1992 152.801 6.43926 142.948 2.8529C133.923 0 125.542 0 108.777 0Z" fill="#C4C4C4"/>
+                </mask>
+                <g mask="url(#mask0_4_732)">
+                <rect width="167" height="167" fill="url(#paint0_linear_4_732)"/>
+                <path d="M61.7708 18.7875H105.229C119.861 18.7875 125.877 18.8541 132.326 20.8648C138.72 23.2381 143.762 28.2799 146.135 34.6746C148.146 41.1219 148.212 47.1396 148.212 61.7708V105.229C148.212 119.861 148.146 125.877 146.135 132.325C143.762 138.721 138.72 143.762 132.326 146.134C125.877 148.146 119.862 148.213 105.229 148.213H61.7708C47.1376 148.213 41.1217 148.146 34.6745 146.134C28.2802 143.762 23.2382 138.721 20.8648 132.326C18.8541 125.877 18.7875 119.861 18.7875 105.229V61.7708C18.7875 47.1396 18.8541 41.1219 20.8647 34.6746C23.238 28.2798 28.2798 23.238 34.6746 20.8647C41.1219 18.8541 47.1384 18.7875 61.7708 18.7875Z" stroke="white" stroke-width="6.95833"/>
+                <rect x="49.4042" y="80.0208" width="68.6555" height="9.80794" rx="4.90397" fill="white"/>
+                <rect x="49.4042" y="103.315" width="68.6555" height="9.80794" rx="4.90397" fill="white"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M73.1441 66.6155C72.6743 66.6155 72.3393 66.1591 72.4533 65.7033C72.7673 64.4476 72.934 63.1335 72.934 61.7806C72.934 60.3168 72.7389 58.8986 72.3732 57.5505C72.2486 57.0911 72.5845 56.6234 73.0605 56.6234H113.064C115.823 56.6234 118.06 58.8602 118.06 61.6194C118.06 64.3787 115.823 66.6155 113.064 66.6155H73.1441Z" fill="white"/>
+                <circle cx="56.7601" cy="61.631" r="7.35595" fill="#F82929" stroke="white" stroke-width="4.175"/>
+                </g>
+                <defs>
+                <linearGradient id="paint0_linear_4_732" x1="83.5" y1="0" x2="83.5" y2="167" gradientUnits="userSpaceOnUse">
+                <stop/>
+                <stop offset="1"/>
+                </linearGradient>
+                </defs>
+            </svg>
+            <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+            <span style={{ color: "#ea4a8f" }}>Ping</span>Back
+            </span>
         </>
         ),
         project: {
             link: "https://github.com/MaximilianHeidenreich/PingBack",
         },
         docsRepositoryBase: "https://github.com/MaximilianHeidenreich/PingBack/blob/main/docs/pages",
-        primaryHue: 308,
+        primaryHue: 334,
         navigation: {
             prev: true,
             next: true
@@ -25,5 +38,8 @@ export default {
             text: <span>
             <span style={{ fontWeight: 800 }}>PingBack</span> | Made with ❤️ by <a href="https://github.com/MaximilianHeidenreich" target="_blank">Maximilian Heidenreich</a>.
             </span>,
+        },
+        sidebar: {
+            defaultMenuCollapseLevel: 2
         }
     }
