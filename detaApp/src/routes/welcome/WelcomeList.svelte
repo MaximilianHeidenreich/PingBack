c49d64ec68f95f64cf11ed83b66b84256aec152a
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-25 15:40:19 +0100

    ✨ Add welcome tour

---
+++ detaApp/src/routes/welcome/WelcomeList.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import Notification from "$cmp/notification/Notification.svelte";
+
+
+
+
+</script>
+
+<div class="wrapper">
+    <ul>
+        <li><Notification /></li>
+        <li><Notification /></li>
+        <li><Notification /></li>
+        <li><Notification /></li>
+        <li><Notification /></li>
+        <li><Notification /></li>
+    </ul>
+</div>
+
+<style lang="postcss">
+    .wrapper {
+        @apply flex justify-center p-6;
+        @apply border rounded-3xl min-h-[40ch] max-h-[40ch] overflow-y-auto bg-blue-50;
+    }
+    .wrapper > ul {
+        @apply flex flex-col gap-3;
+    }
+</style>
