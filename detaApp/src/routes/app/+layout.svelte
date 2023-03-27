d01ceb13fddd7445f818d81c2d5c4183e8ed0fbe
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-27 22:15:04 +0200

    👔 Hide aside on mobile by default

--- detaApp/src/routes/app/+layout.svelte
+++ detaApp/src/routes/app/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     import App from "$cmp/core/scaffold/App.svelte";
     import { s_appSidebarCollapsed } from "$cmp/core/scaffold/s_appSidebarCollapsed";
+    import { isMobile } from "$lib/utils/width";
     import { onMount } from "svelte";
 
     onMount(() => {
-        const width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-        s_appSidebarCollapsed.set(width < 768);
+        s_appSidebarCollapsed.set(isMobile());
     })
 </script>
 
