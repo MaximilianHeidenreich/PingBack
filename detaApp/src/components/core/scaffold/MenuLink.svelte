93f1f537e958b024e2dae5ef9f4298eb89a20b6d
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-27 21:09:35 +0200

    💄 Improve menu link hover

--- detaApp/src/components/core/scaffold/MenuLink.svelte
+++ detaApp/src/components/core/scaffold/MenuLink.svelte
@@ -10,15 +10,14 @@
 <style lang="postcss">
     a {
         @apply flex w-full justify-start items-center gap-3 rounded-xl py-2.5 px-4;
+        @apply font-medium text-neutral-700 border-2 border-transparent;
         @apply transition-colors duration-200 ease-in-out;
-
-        @apply font-medium text-neutral-700;
     }
     a:not(.disabled).active {
         @apply bg-neutral-100;
     }
-    a:not(.disabled):hover {
-        @apply bg-neutral-100;
+    a:not(.disabled, .active):hover {
+        @apply border-neutral-200; /*bg-neutral-100;*/
     }
     a:not(.disabled):active {
         @apply bg-neutral-200;
