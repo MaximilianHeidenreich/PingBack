23da63dce36748cc0aa5453606fa839204da017a
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-27 20:50:48 +0200

    ✨ Auto hide aside on mobile

--- detaApp/src/routes/app/+layout.svelte
+++ detaApp/src/routes/app/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
     import App from "$cmp/core/scaffold/App.svelte";
+    import { s_appSidebarCollapsed } from "$cmp/core/scaffold/s_appSidebarCollapsed";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        const width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+        s_appSidebarCollapsed.set(width < 768);
+    })
 </script>
 
 <App>
