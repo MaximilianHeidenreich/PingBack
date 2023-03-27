fa21a5a730144fad96faa6e1b198c6ccc9c67489
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-27 20:51:21 +0200

    👔 Add darkMode store

--- detaApp/src/components/core/buttons/ThemeToggle.svelte
+++ detaApp/src/components/core/buttons/ThemeToggle.svelte
@@ -2,25 +2,23 @@
     import Sun1 from "iconsax-svelte/Sun1.svelte";
     import Moon from "iconsax-svelte/Moon.svelte";
     import IconButton from "./IconButton.svelte";
-
-    // STATE
-    let isDarkMode = false;
+    import { s_darkMode } from "$lib/stores/s_darkMode";
+    import { get } from "svelte/store";
 
     // HANDLERS
     function onToggle() {
-        isDarkMode = !isDarkMode;
-        isDarkMode
+        s_darkMode.set(!get(s_darkMode));
+        $s_darkMode
             ? document.documentElement.classList.add("dark")
             : document.documentElement.classList.remove("dark");
 
     }
-
 </script>
 
 
 <IconButton
     on:click={onToggle}>
-    {#if isDarkMode}
+    {#if $s_darkMode}
     <Sun1 size={20}/>
     {:else}
     <Moon size={20}/>
