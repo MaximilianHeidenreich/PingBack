bf687c1b61d3bf9eb39bf7c1c8f356cb48b8492c
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-27 22:13:58 +0200

    👔 Add localStore svelte store

---
+++ detaApp/src/lib/stores/localStorable.ts
@@ -0,0 +1,32 @@
+import { browser } from "$app/environment";
+import { get, writable, type Updater } from "svelte/store";
+
+/**
+ * Svelte writable store connected to localStorage API for persiatance.
+ * @param data
+ * @param localStoreKey
+ * @returns
+ */
+export function localStorable<T>(data: T, localStoreKey: string) {
+   const store = writable<T>(data);
+   const { subscribe, set, update } = store;
+
+   browser &&
+      localStorage.getItem(localStoreKey) &&
+      set(JSON.parse(localStorage.getItem(localStoreKey)!));
+
+   return {
+      subscribe,
+      set: (d: T) => {
+         browser && (localStorage.storable = JSON.stringify(d));
+         set(d);
+      },
+      update: (updater: Updater<T>) => {
+        update(updater);
+         const updatedStore = get(store);
+
+         browser && (localStorage.storable = JSON.stringify(updatedStore));
+         set(updatedStore);
+      }
+   };
+}
