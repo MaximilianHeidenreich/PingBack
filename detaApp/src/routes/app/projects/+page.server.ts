5b021451e614f95445744ccc27d20ec56f75620a
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-03-29 10:04:50 +0200

    ⚡️ Concurrent fetch

--- detaApp/src/routes/app/projects/+page.server.ts
+++ detaApp/src/routes/app/projects/+page.server.ts
@@ -1,28 +1,16 @@
 import { db_apiKeys, db_projects } from "$lib/server/deta";
-import type { IApiKey } from "$lib/types/IApiKey";
-import type { IProject } from "$lib/types/IProject";
 import type { PageServerLoad } from "./$types";
 
-export const load = (async () => {
+export const load = (async ({ depends }) => {
+    depends("/app/settings");
 
-    let projects: IProject[] | null;
-    try {
-        projects = (await db_projects.fetch({}, {})).items; // TODO: Switch to fetchAll
-    } catch (err) {
-        console.error(err);
-        projects = null;
-    }
-
-    let apiKeys: IApiKey[] | null;
-    try {
-        apiKeys = (await db_apiKeys.fetch({}, {})).items; // TODO: Switch to fetchAll
-    } catch (err) {
-        console.error(err);
-        apiKeys = null;
-    }
+    const [projectsRes, apiKeysRes] = await Promise.all([
+        db_projects.fetch({}, {}),
+        db_apiKeys.fetch({}, {})
+    ]);
 
     return {
-        projects,
-        apiKeys
+        projects: projectsRes.items,
+        apiKeys: apiKeysRes.items
     };
 }) satisfies PageServerLoad;
