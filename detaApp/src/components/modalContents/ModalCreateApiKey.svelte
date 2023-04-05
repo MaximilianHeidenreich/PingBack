27feeab616370774f525ea43203fe85204d8c130
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-04-05 22:36:00 +0200

    ✨ Add create & delete api key modals

--- detaApp/src/components/modalContents/ModalCreateApiKey.svelte
+++ detaApp/src/components/modalContents/ModalCreateApiKey.svelte
@@ -2,7 +2,9 @@
     import Button from "$cmp/core/buttons/Button.svelte";
     import Input from "$cmp/core/inputs/Input.svelte";
     import Modal from "$cmp/core/modals/Modal.svelte";
-    import { sanitizeApiKeyName } from "$lib/utils/sanitizers";
+    import { client_CreateApiKey } from "$lib/client/apiKey";
+    import { NotFound } from "$lib/errors/core";
+    import { sanitizeApiKeyNameInput } from "$lib/utils/sanitizers";
     import toastOptions from "$lib/utils/toast";
     import toast from "svelte-french-toast";
 
@@ -11,7 +13,7 @@
 
     // STATE
     let dialog: HTMLDialogElement;
-    let keyName: string;
+    let keyName: string; // TODO: Rename to name
     let working = false;
 
     // HANDLERS
@@ -19,31 +21,20 @@
         dialog.close();
     }
     async function onCreate() {
-        onCreated && onCreated();
-        // TODO: impl
-        /*const res = await clientCreateProjectRaw(fetch, {
-            key: sanitizeProjectIdInput(projectName),
-            displayName: projectName,
-        });
-        console.debug("Create project fetch result", res);
-
-        if (res.status === 200) {
-            toast.success("Created project!", toastOptions());
-            dialog.close();
-            const body = (await res.json()) as IProject;
-            s_appSidebarProjects.update(projects => {
-                projects = [...projects, body];
-                return projects;
-            });
+        working = true;
+        try {
+            const res = await client_CreateApiKey(fetch, { project: projectKey, displayName: keyName });
+        } catch (e) {
+            toast.error("Could not create API key!", toastOptions());
+            if (e instanceof NotFound) alert(e);
+            console.error(e);
+            working = false;
             return;
         }
-        else if (res.status === 400) {
-            toast.error("Could not create project!", toastOptions());
-        }
-        else if (res.status === 409) {
-            toast.error("Prokect already exists!", toastOptions());
-        }
-        */
+        toast.success("Created API key!", toastOptions());
+        dialog.close();
+        onCreated && onCreated();
+        working = false;
     }
 </script>
 
@@ -63,12 +54,12 @@
                 <span class="text-gray-700">Key Name</span>
                 <Input
                     bind:value={keyName}
-                    sanitizer={sanitizeApiKeyName}
+                    sanitizer={sanitizeApiKeyNameInput}
                     placeholder="Example Application" />
             </label>
             <p class="mt-2 px-0.5 leading-5">
                 <small class="mt-1">
-                    Use a descriptive name for your key. This will help you identify it later.
+                    Use a descriptive name for your key (max. 25 chars). <br>This will help you identify it later.
                 </small>
             </p>
         </fieldset>
