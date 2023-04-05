e5f48f130e951304202a1b7c387af27315373a76
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-04-05 22:37:29 +0200

    docs fmt

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,6 @@
-const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx',
+const withNextra = require("nextra")({
+    theme: "nextra-theme-docs",
+    themeConfig: "./theme.config.jsx",
 })
 
 module.exports = withNextra({
@@ -8,7 +8,7 @@ module.exports = withNextra({
     images: {
         unoptimized: true
     }
-})
+});
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
