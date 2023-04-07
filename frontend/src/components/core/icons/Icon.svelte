6f0a9e6fc24835a8a65510b00a947b91fe159ede
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-04-07 02:12:23 +0200

    👔 Add svg Icon component

--- frontend/src/components/core/icons/Icon.svelte
+++ frontend/src/components/core/icons/Icon.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
-    // PROPS
-    export let size: number = 24, stroke: number = 1, style: "black" | "white" = "black";
+// PROPS
+    export let size: number = 24, 
+        strokeWidth: number = 1, 
+        style: "light" | "dark" = "dark",
+        fill: string | undefined = undefined;
 </script>
 
-<slot/>
+<svg width="{size}" height="{size}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14" stroke-width="{strokeWidth}" class="icon {style}">
+    <slot/>
+</svg>
+
+
+
+<style lang="postcss">
+    .icon {
+
+    }
+    .icon.dark {
+        color: black;
+    }
+    .icon.light {
+        color: white;
+    }
+</style>
