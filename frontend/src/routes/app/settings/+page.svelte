dda6e4b157fcb72bb88fbbda2872bb174809f4c1
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-04-07 02:14:05 +0200

    ✨ Add time format setting

--- frontend/src/routes/app/settings/+page.svelte
+++ frontend/src/routes/app/settings/+page.svelte
@@ -10,6 +10,7 @@
     import { s_sysContentHash } from "$lib/stores/s_sysContentHash";
     import { sw_register } from "$lib/utils/serviceWorker";
     import { onMount } from "svelte";
+    import { s_timeFormat } from "$lib/stores/s_timeFormat";
 
     // STATE
     let dbInfo: PouchDB.Core.DatabaseInfo | null | undefined;
@@ -41,16 +42,23 @@
     <!--<AppContentSection>
         <span class="text-lg font-medium">Notifications</span>
         <hr class="mt-2 mb-4">
-        <div class="flex items-center gap-2">
-            <span class="mb-0.5 font-medium">Push Notifications: </span>
-            <span class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-green-100 bg-green-500 rounded-full">Enabled</span>
-        </div>
-        <Button on:click={onEnablePushNotifications}>Enable Push Notifications</Button>
-        <div class="flex items-center gap-2">
-            <span class="mb-0.5 font-medium">Web App Notifications: </span>
-            <span class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-green-100 bg-green-500 rounded-full">Enabled</span>
-        </div>
-        status OR enable / disable
+    <AppContentSection>
+        <span class="text-lg font-medium">Display</span>
+        <hr class="mt-2 mb-4">
+        
+        <ul class="flex flex-col gap-6">
+            <li>
+                <ul class="flex items-center gap-3">
+                    <li class="mb-0.5 font-medium"><span>Timestamp Format: </span></li>
+                    <li>
+                        <ul class="flex items-center">
+                            <li><button on:click={() => s_timeFormat.set("relative")} class="bg-neutral-50 border rounded-l-xl px-4 py-1 {$s_timeFormat === 'relative' ? '!bg-[#2F2B43] text-white' : ''}">Relative</button></li>
+                            <li><button on:click={() => s_timeFormat.set("absolute")} class="bg-neutral-50 border border-l-0 rounded-r-xl px-4 py-1 {$s_timeFormat === 'absolute' ? '!bg-[#2F2B43] text-white' : ''}">Absolute</button></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
     </AppContentSection>
     <AppContentSection>
         <AppContentSectionHeader>
