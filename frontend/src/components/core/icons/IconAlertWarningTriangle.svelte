faef010060c35b840f43e64e516661e67e35d005
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-04-07 02:12:36 +0200

    💄 Add some icons

---
+++ frontend/src/components/core/icons/IconAlertWarningTriangle.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Icon from "./Icon.svelte";
+</script>
+
+<Icon {...$$restProps}>
+<path stroke="currentColor" stroke-linejoin="round" d="M1.255 10.4795L5.20393 2.41708C5.93553 0.923392 8.06458 0.923393 8.79618 2.41708L12.7451 10.4795C12.918 10.8325 13.0558 11.2289 12.7035 11.5097C11.0469 12.8301 2.95322 12.8301 1.29661 11.5097C0.9438 11.2285 1.08026 10.8362 1.255 10.4795Z"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M7 5V7.5"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M7 9.75L7 10"></path>
+</Icon>
