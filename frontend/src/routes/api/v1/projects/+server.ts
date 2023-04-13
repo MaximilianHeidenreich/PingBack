bc90eeec231e7eba47bee321960c0ff433566d02
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-04-13 18:12:49 +0200

    🏗️ Move project api logic to extracted

--- frontend/src/routes/api/v1/projects/+server.ts
+++ frontend/src/routes/api/v1/projects/+server.ts
@@ -1,4 +1,6 @@
+import { Invalid, InvalidZod } from "$lib/errors/core";
 import { db_projects } from "$lib/server/deta";
+import { server_CreateProject } from "$lib/server/project";
 import { buildResponse, respondBadRequest, respondInternalError } from "$lib/server/responseHelper";
 import type { IProject } from "$lib/types/IProject";
 import { sanitizeProjectIdInternal } from "$lib/utils/sanitizers";
@@ -37,13 +39,21 @@ export const GET = (async ({ url }) => {
 
 export const POST = (async ({ request }) => {
     const reqBody = await request.json();
-
-    // TOOD: Validate
-
     const displayName = reqBody.displayName.trim();
-    const key = sanitizeProjectIdInternal(reqBody.key);
 
-    let pendingProject: Omit<IProject, "key"> = {
+
+    let newProject: IProject;
+    try {
+        newProject = await server_CreateProject(reqBody);
+    } catch (e) {
+        if (e instanceof InvalidZod) return respondBadRequest(e);
+        if (e instanceof Invalid) return respondBadRequest(e); // TODO: better response
+        console.error(e)
+        return respondInternalError(e as string);
+    }
+
+
+    /*let pendingProject: Omit<IProject, "key"> = {
         createdAt: Date.now(),
         contentHash: crypto.randomUUID(),
         latestEventTimestamp: Date.now(),
@@ -57,11 +67,11 @@ export const POST = (async ({ request }) => {
     } catch (err) {
         // TODO: only handle duplicate key error
         return respondBadRequest("Project key already exists!");
-    }
+    }*/
 
     return buildResponse()
         .status(200)
         .statusText("OK")
-        .json({ ...pendingProject, key })
+        .json(newProject)
         .build();
 }) satisfies RequestHandler;
