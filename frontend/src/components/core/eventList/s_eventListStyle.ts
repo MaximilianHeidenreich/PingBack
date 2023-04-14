6d72a521501e8176c8409d3fe7b20b594a89f356
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-04-14 12:43:58 +0200

    add pb_ prefix

--- frontend/src/components/core/eventList/s_eventListStyle.ts
+++ frontend/src/components/core/eventList/s_eventListStyle.ts
@@ -1,3 +1,3 @@
 import { storedWritable } from "$lib/stores/storedWritable";
 
-export const s_eventListStyle = storedWritable<"compact" | "card">("eventListStyle", "compact");
+export const s_eventListStyle = storedWritable<"compact" | "card">("pb_eventListStyle", "compact");
