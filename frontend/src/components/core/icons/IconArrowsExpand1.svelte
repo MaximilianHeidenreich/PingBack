a7f85a7a863f41ba34f297739e8a2400e15c1f99
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-04-29 23:50:28 +0200

    💄 Add expand & shrink icons

---
+++ frontend/src/components/core/icons/IconArrowsExpand1.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Icon from "./Icon.svelte";
+</script>
+
+<Icon {...$$restProps}>
+<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M5.10707 5.10717L1.0956 1.0957M5.15252 1.09201C3.53191 0.931056 2.64179 0.930964 1.09194 1.09201C0.930895 2.64186 0.930987 3.53198 1.09194 5.15259"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M5.10708 8.95457L1.08743 12.9742M1.09194 8.90913C0.930987 10.5297 0.930895 11.4199 1.09194 12.9697C2.64179 13.1308 3.53191 13.1307 5.15252 12.9697"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M12.9727 12.9727L9.00007 9M8.90918 12.9697C10.5298 13.1307 11.4199 13.1308 12.9698 12.9697C13.1308 11.4199 13.1307 10.5298 12.9698 8.90914"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M12.9141 1.08592L9.00004 4.99998M12.9698 5.15261C13.1307 3.53199 13.1308 2.64187 12.9698 1.09203C11.4199 0.930979 10.5298 0.931071 8.90918 1.09203"></path>
+</Icon>
---
+++ frontend/src/components/core/icons/IconArrowsShrink1.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Icon from "./Icon.svelte";
+</script>
+
+<Icon {...$$restProps}>
+<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M5.10547 8.89453L1.01965 12.9804"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M12.887 1.05133L8.76409 5.1742"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M5.22864 5.22853L0.993408 0.993301"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M12.9204 12.9204L8.76562 8.76562"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M5.10669 12.954C5.26765 11.3333 5.26774 10.4432 5.10669 8.89338C3.55685 8.73234 2.66672 8.73243 1.04611 8.89338"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M1.16626 5.22697C2.78688 5.38793 3.677 5.38802 5.22684 5.22698C5.38789 3.67713 5.3878 2.78701 5.22684 1.16639"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M8.76831 1.1664C8.60735 2.78701 8.60726 3.67713 8.76831 5.22698C10.3182 5.38803 11.2083 5.38793 12.8289 5.22698"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M12.8289 8.75038C11.2082 8.58943 10.3181 8.58933 8.76828 8.75038C8.60723 10.3002 8.60732 11.1903 8.76828 12.811"></path>
+</Icon>
